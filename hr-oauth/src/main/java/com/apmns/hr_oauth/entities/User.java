ad752ba353b7dcc1edfeb7c7aa30d23845c1828c
amanda nascimento <apmnsilva@gmail.com>
2024-07-11 11:28:15 -0300

    Login config

--- hr-oauth/src/main/java/com/apmns/hr_oauth/entities/User.java
+++ hr-oauth/src/main/java/com/apmns/hr_oauth/entities/User.java
@@ -1,17 +1,20 @@
 package com.apmns.hr_oauth.entities;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User implements Serializable {
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class User implements UserDetails, Serializable {
 
     private Long id;
     private String name;
     private String email;
     private String password;
-    
+
     private Set<Role> roles = new HashSet<>();
 
     public User() {
@@ -48,8 +51,39 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
